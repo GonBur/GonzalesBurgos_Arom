ae20dca277a2cdce7f2a08f696788e7c806b73b1
Arom Burgos <gonzalesburgosaromsadith2004@gmail.com>
2023-02-12 21:44:01 -0500

    actualizando cambios de la pagina sobre mi 1.2

--- js/js.js
+++ js/js.js
@@ -1,17 +1,20 @@
 let boton = document.getElementById("boton");
-let nav=document.getElementById("nav");
+let nav = document.getElementById("nav");
 
-boton.addEventListener("click", function(){
-    let val=nav.classList.toggle("activo")
-    localStorage.setItem("boton",val)
-})
+/* funcion para mostrar el menu */
+function mostrarMenu(){
+    nav.classList.toggle("activo")
+    /* localStorage.setItem("boton", mostrar)   */ 
+}
 
-let valor=localStorage.getItem("activo")
+boton.addEventListener("click", mostrarMenu)
+
+/* let valor=localStorage.getItem("activo")
 
 if (valor=="true") {
     nav.classList.add("activo")
 } else {
     nav.classList.remove("activo")
-}
+ */
 
 
